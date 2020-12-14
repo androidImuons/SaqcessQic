bddb19067ea1bc993ac489bcf1a77ce782dd11c5
Admin <rahul.vhayaskar@imuons.com>
2020-12-14 16:00:54 +0530

    init

--- src/main/java/com/saqcess/qicpic/app/utils/MyApplication.java
+++ src/main/java/com/saqcess/qicpic/app/utils/MyApplication.java
@@ -1,15 +1,16 @@
 package com.saqcess.qicpic.app.utils;
 
 import android.app.Application;
+import android.content.Context;
 
 public class MyApplication extends Application {
 
     private static MyApplication mInstance;
-
+public static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
-
+context=getApplicationContext();
         mInstance = this;
     }
 
